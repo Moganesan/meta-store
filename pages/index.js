dace0436dcc629258ab900102f5f497c87c2ea61
Moganesan <developer.moganesan@gmail.com>
2022-06-28 18:33:15 +0530

    category products

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import Banner from "../components/banner";
 import Categories from "../components/categories";
 import FeaturedBrands from "../components/featuredBrands";
 import TodaysDeal from "../components/todaysDeal";
+import FeaturedProducts from "../components/featuredProducts";
+import CategoryProducts from "../components/categoryProducts";
 
 export default function Home() {
   return (
@@ -15,6 +17,8 @@ export default function Home() {
       <Categories />
       <FeaturedBrands />
       <TodaysDeal />
+      <FeaturedProducts />
+      <CategoryProducts category={"Fruits & Vegitables"} />
     </div>
   );
 }
